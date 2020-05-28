34151131cc1fa9233fc6850736139c0fd820ea04
Joe-J-Salvatore <jjsalvatore@gmail.com>
2020-05-28 16:54:54 -0700

    Set app for Heroku

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,18 @@
 // fetch weather forecast
-const url = 'http://localhost:3000/weather?address=';
+const url = '/weather?address=';
 // unsplash photo API
 const access_key = 'cPlfRcFWfrcEgMSxtxDGX-AcTtwvp5HFhfSMjG_n6Dw';
 const secret_key = 'ij3ulntj_w9hZm65bZooFMs1Thigd-KVUh5Gi8UrDjA';
 // get search form
 const weatherForm = document.querySelector('.weather-search');
 const search = document.querySelector('input');
+// html to display data
 const loc = document.getElementById('city');
 const forecast = document.getElementById('forecast');
 const para = document.getElementById('conditions');
-//const iconDiv = document.getElementById('icon');
 const photo = document.getElementById('photo');
 
+// access user input to get weather data from browser
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = search.value;
@@ -41,7 +42,9 @@ weatherForm.addEventListener('submit', (e) => {
       }
     });
   });
+  // access unsplash photo API
   const unsplashUrl = `https://api.unsplash.com/search/photos?query=${location} skyline&orientation=landscape&page=1&client_id=${access_key}`;
+
   fetch(unsplashUrl).then(response => {
     response.json().then(data => {
       const pic = document.createElement('IMG');
--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 const hbs = require('hbs');
 const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // view engine
 app.set('view engine', 'hbs');
